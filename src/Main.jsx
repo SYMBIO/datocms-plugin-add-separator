d9264b5eea8ba588616922ac0470aa3e7db00f24
Martin Cajthaml <martin.cajthaml@symbio.agency>
2019-09-16 13:45:52 +0200

    initial import

---
+++ src/Main.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import connectToDatoCms from './connectToDatoCms';
+import './style.css';
+
+@connectToDatoCms(plugin => ({
+  developmentMode: plugin.parameters.global.developmentMode,
+  caption: plugin.parameters.instance.caption,
+}))
+
+export default class Main extends Component {
+  static propTypes = {
+    caption: PropTypes.bool.isRequired,
+  };
+
+  render() {
+    const { caption } = this.props;
+    console.log(caption);
+    return (
+      <h1>{caption}</h1>
+    );
+  }
+}
